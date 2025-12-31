6cf345c38d2daca3d4e5c0cf88fe2f3980cf5f04
sukritdutta234 <artemisofficial10@gmail.com>
2025-12-31 18:16:22 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+package javabasics;
+
+public class FirstProgram {
+    public static void main(String[] args){
+        //printing basic statements
+        System.out.println("Hello!, welcome to my java-basics repo");
+        System.out.println("This is just the beginning...");
+        System.out.println("A lot to know and lot implement!!!");
+    }
+}
---
+++ PrimitveDataTypes.java
@@ -0,0 +1,19 @@
+package javabasics;
+
+public class PrimitveDataTypes {
+    public static void main(String[] args){
+
+        //declaration and assignment of primitive datatypes
+
+        byte b1 = 123; // 8 bit memory allocation
+        short sh1 = 5678; //16 bit
+        int num1 = 188899; //32 bit
+        long l1 = 190933094; //64 bit
+        float f1 = 12.35642f; // 32bit
+        double d1 = 1293980.283699022; // 64 bit
+        char ch1 = 'A'; //16 bit
+        String str1 = "I love JAVA";
+        boolean b2 = true; //1 bit
+        System.out.println(f1);
+    }
+}
