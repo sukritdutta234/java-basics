9c3fc4a76c804c6aa521efdeeaa1f0e8720be7b1
sukritdutta234 <artemisofficial10@gmail.com>
2026-01-01 12:09:03 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,17 @@
+package javabasics;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        // ==
+        // !=
+        // >= <= < >
+        int num1 , num2;
+        num1 = 1999;
+        num2 = 1999;
+        if (num1 == num2){
+            System.out.println("both are equal");
+        }
+        
+
+    }
+}
